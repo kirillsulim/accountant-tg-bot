bc60529841654578d9e17c386e824b9c43816564
Kirill Sulim <kirillsulim@gmail.com>
2023-06-17 00:31:19 +0500

    Small test refactoring

--- tests/db/test_session_factory.py
+++ tests/db/test_session_factory.py
@@ -9,21 +9,21 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from db.session_factory import SessionFactory
 
 
-class DemoService:
+class TestService:
     def __init__(self, session_factory: SessionFactory):
         self.session_factory = session_factory
 
     async def select_1(self) -> Awaitable[int]:
-        async def select_1(session: AsyncSession) -> Awaitable[int]:
+        async def select_1(session: AsyncSession) -> int:
             result = await session.execute(text("SELECT 1;"))
             return result.first()[0]
 
-        return self.session_factory.run_with_session(select_1)
+        return await self.session_factory.run_with_session(select_1)
 
 
 class TestDbAsync:
     @pytest.mark.asyncio
     async def test_bla(self):
-        ds = DemoService(session_factory=SessionFactory("sqlite+aiosqlite://"))
+        ds = TestService(session_factory=SessionFactory("sqlite+aiosqlite://"))
         rs = await ds.select_1()
-        print(await rs)
+        assert rs == 1
